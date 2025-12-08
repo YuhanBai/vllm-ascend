9ea4b7d26a1e3337b92467e943679c9e53679bdc
YuhanBai <yuhan.bai0830@gmail.com>
2025-12-08 16:17:57 +0800

    [Draft] Format Fix.
    Signed-off-by: YuhanBai <yuhan.bai0830@gmail.com>

--- vllm_ascend/ops/layernorm.py
+++ vllm_ascend/ops/layernorm.py
@@ -71,7 +71,8 @@ def _addrmsnorm_forward_oot(
 
     else:
         if get_ascend_device_type(
-        ) == AscendDeviceType._310P or envs_ascend.T_I_CONSISTENCY:
+        ) == AscendDeviceType._310P or \
+                envs_ascend.T_I_CONSISTENCY:
             orig_dtype = residual.dtype
             x = x + residual.to(x.dtype)
             residual = x.to(orig_dtype)
@@ -202,8 +203,7 @@ class AscendGemmaRMSNorm(GemmaRMSNorm):
 
         from vllm_ascend.utils import AscendDeviceType, get_ascend_device_type
         if residual is not None:
-            if get_ascend_device_type(
-            ) == AscendDeviceType._310P or envs_ascend.T_I_CONSISTENCY:
+            if get_ascend_device_type() == AscendDeviceType._310P:
                 orig_dtype = residual.dtype
                 x = x + residual.to(x.dtype)
                 residual = x.to(orig_dtype)

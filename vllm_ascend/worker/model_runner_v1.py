8e500253b0662be902d05bbe1271b43b42c1a0e0
YuhanBai <yuhan.bai0830@gmail.com>
2025-12-17 14:55:10 +0800

    [Draft] _dummy_sampler_runner
    Signed-off-by: YuhanBai <yuhan.bai0830@gmail.com>

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -73,6 +73,7 @@ from vllm.v1.outputs import (EMPTY_MODEL_RUNNER_OUTPUT, AsyncModelRunnerOutput,
                              make_empty_encoder_model_runner_output)
 from vllm.v1.sample.metadata import SamplingMetadata
 from vllm.v1.sample.rejection_sampler import RejectionSampler
+from vllm.v1.sample.logits_processor import LogitsProcessors
 from vllm.v1.spec_decode.metadata import SpecDecodeMetadata
 from vllm.v1.spec_decode.ngram_proposer import NgramProposer
 from vllm.v1.spec_decode.suffix_decoding import SuffixDecodingProposer
@@ -2188,8 +2189,8 @@ class NPUModelRunner(GPUModelRunner):
             isinstance(hidden_states[0], torch.Tensor):
             hidden_states = hidden_states[0]
             hidden_states = hidden_states[logit_indices]
-        logits = self.model.compute_logits(hidden_states)
         num_reqs = logits.size(0)
+
         dummy_tensors = lambda v: torch.full((num_reqs,), v, device=self.device)
 
         dummy_metadata = SamplingMetadata(
@@ -2203,7 +2204,13 @@ class NPUModelRunner(GPUModelRunner):
             no_penalties=True,
             prompt_token_ids=None,
             frequency_penalties=dummy_tensors(0.1),
-            
+            presence_penalties=dummy_tensors(0.1),
+            repetition_penalties=dummy_tensors(0.1),
+            output_token_ids=[[] for _ in range(num_reqs)],
+            spec_token_ids=[[] for _ in range(num_reqs)],
+            allowed_token_ids_mask=None,
+            bad_words_token_ids={},
+            logitsprocs=LogitsProcessors(),
         )
         try:
             output = self.sampler(
@@ -2212,10 +2219,33 @@ class NPUModelRunner(GPUModelRunner):
         except RuntimeError as e:
             if "out of memory" in str(e):
                 raise RuntimeError(
-                    ""
-                )
+                    "NPU out of memory occurred when warming up sampler with "
+                    f"{num_reqs} dummy requests. Please try lowering "
+                    "`max_num_seqs` or `gpu_memory_utilization` when "
+                    "initializing the engine."
+                ) from e
             else:
                 raise e
+        if self.speculative_config:
+            draft_token_ids = [[0] for _ in range(num_reqs)]
+            dummy_spec_decode_metadata = SpecDecodeMetadata.make_dummy(
+                draft_token_ids, self.device
+            )
+
+            num_tokens = sum(len(ids) for ids in draft_token_ids)
+            draft_probs = None
+            logits = torch.randn(
+                num_tokens + num_reqs,
+                logits.shape[-1],
+                device=self.device,
+                dtype=logits.dtype,
+            )
+            self.rejection_sampler(
+                dummy_spec_decode_metadata,
+                draft_probs,
+                logits,
+                dummy_metadata,
+            )
         return output
 
     def profile_run(self) -> None:
